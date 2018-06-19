2956c0f1ad1a5739778c2d86d0f036be2bbe61da
anzhiruodu <358270236@qq.com>
2018-06-19 23:31:06 +0800

    修改

--- lishenmingsecurity/lishenming-security-demo/src/main/java/com/lishenming/web/controller/TimeAspect.java
+++ lishenmingsecurity/lishenming-security-demo/src/main/java/com/lishenming/web/controller/TimeAspect.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Component;
 /**
  * 切片
  * spring官网11章aop切片；详细讲解
+ * 可以拿到传入的参数的值，，
  */
 @Aspect
 @Component
@@ -16,7 +17,7 @@ public class TimeAspect {
     @Around("execution(* com.lishenming.web.controller.UserController.*(..))")
     public Object handleControllerMethod(ProceedingJoinPoint pjp) throws Throwable {
         System.out.println("aspect start");
-        Object proceed = pjp.proceed();
+        Object proceed = pjp.proceed();//返回的是什么类型，那么就可以强转为什么类型，例如返回的是map，
         System.out.println("aspect end");
 
         return null;
--- lishenmingsecurity/lishenming-security-demo/src/main/java/com/lishenming/web/controller/config/WebConfig.java
+++ lishenmingsecurity/lishenming-security-demo/src/main/java/com/lishenming/web/controller/config/WebConfig.java
@@ -1,11 +1,17 @@
 package com.lishenming.web.controller.config;
 
+import com.lishenming.filter.TimeFilter;
 import com.lishenming.web.controller.interceptor.TimeInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 异步开启拦截器
@@ -17,7 +23,7 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     private TimeInterceptor timeInterceptor;
 
     /**
-     * 两种异步的方式
+     * 两种异步的方式，
      * registerCallableInterceptors
      * registerDeferredResultInterceptors
      *
@@ -27,10 +33,32 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     public void configureAsyncSupport(AsyncSupportConfigurer configurer) {
 //        configurer.registerDeferredResultInterceptors();
         //configurer.registerCallableInterceptors()
+        //configurer.setTaskExecutor()//自定义线程池，这个可以重用，spring(DeferredResult)自带的不会重用DeferredResult
     }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(timeInterceptor);
     }
+
+    /**
+     *
+     * @return
+     * 这个配置，可以自定义路径，拦截器在指定的路径起作用，
+     * 但是也可以在TimeFilter上面加入注解@conponet，也可以将拦截器加入到容器中，但是不能自定义
+     */
+//    @Bean
+    public FilterRegistrationBean timeFilter(){
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        TimeFilter timeFilter = new TimeFilter();
+        filterRegistrationBean.setFilter(timeFilter);
+        List<String> urls = new ArrayList<>();
+        urls.add("/*");
+        filterRegistrationBean.setUrlPatterns(urls);
+        return filterRegistrationBean;
+    }
+
+
+
+
 }
