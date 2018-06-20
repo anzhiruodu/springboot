f52c06a52c0231aa9e03b2ef600148e11c89f9c2
anzhiruodu <358270236@qq.com>
2018-06-21 00:17:46 +0800

    地方

---
+++ lishenmingsecurity/lishenming-security-core/src/main/java/com/lsm/security/core/properties/BrowserProperties.java
@@ -0,0 +1,13 @@
+package com.lsm.security.core.properties;
+
+public class BrowserProperties {
+    private String loginPage = "/demo-signIn.html";
+
+    public String getLoginPage() {
+        return loginPage;
+    }
+
+    public void setLoginPage(String loginPage) {
+        this.loginPage = loginPage;
+    }
+}
